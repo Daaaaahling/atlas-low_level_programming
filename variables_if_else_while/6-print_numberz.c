8bd347cf21cb2d0e604bcf6d58f99b07697cc4f4
Daaaaahling <una_cruz@yahoo.com>
2024-01-20 10:59:07 -0800

    corrected error

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 /**
- * main - Prints Numbers
+ * main - prints all single digit numbers of base 10
  *
  * Return: 0
- * /
- * int main(void)
- * {
- * int x;
- *
- * for (x = 0; x < 10; x++)
- * }
- * putchar(x + '0');
- * if (x < 9)
- * {
- * }
- * }
- * putchar('\n');
- * return (0);
- * }
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+		if (x < 9)
+		{
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
