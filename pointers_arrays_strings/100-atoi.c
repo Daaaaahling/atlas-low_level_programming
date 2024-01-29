852d107d66d5379e737409f4ec447bfe0b847a27
Daaaaahling <una_cruz@yahoo.com>
2024-01-29 13:03:01 -0800

    edited formatting

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,30 +13,33 @@ int _atoi(char *s)
 
 	while (*s)
 	{
+		if (*s == ' ')
+		{
+			s++;
+		}
+		else if (*s == '-')
+		{
+			sign = -sign;
+			s++;
+		}
+		else if (*s == '+')
+		{
+			s++;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			break;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+	while (*s >= '0' && *s <= '9')
+	{
+		result = result * 10 + (*s - '0');
 		s++;
 	}
-	else if (*s == '-')
-	{
-		sign = -sign;
-		s++;
-	}
-	else if (*s == '+')
-	{
-		s++;
-	}
-	else if (*s >= '0' && *s <= '9')
-	{
-		break;
-	}
-	else
-	{
-		return (0);
-	}
-}
-while (*s >= '0' && *s <= '9')
-{
-	result = result * 10 + (*s - '0');
-	s++;
-}
-return (result * sign);
+	return (result * sign);
 }
