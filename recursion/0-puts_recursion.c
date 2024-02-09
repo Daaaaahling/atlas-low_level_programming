90ddc231dc30b71ec7c2e4006e6034c0002ba202
Daaaaahling <una_cruz@yahoo.com>
2024-02-09 11:55:29 -0800

    shortened line

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts_recursion - writes a function that prints a string followed by a new line
+ * _puts_recursion - function that prints a string followed by a new line
  * @s: string to be printed
  */
 
