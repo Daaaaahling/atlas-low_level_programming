f3f85c1f27ec891a148d575dbcbddcbd8101546d
Daaaaahling <una_cruz@yahoo.com>
2024-02-10 13:50:08 -0800

    edited main.h

--- recursion/main.h
+++ recursion/main.h
@@ -1,26 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char);
-int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int _abs(int n);
-int _isupper(int c);
-int _isdigit(int c);
-int _strlen(char *s);
-void _puts(char *s);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -30,5 +9,4 @@ int _sqrt_recursion(int n);
 int find_sqrt(int n, int guess);
 int is_prime_number(int n);
 int prime_helper(int, int);
-
 #endif
