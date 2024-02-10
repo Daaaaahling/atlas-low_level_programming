bd1db6167aa4833fb5d7d6a95ccf524b187cd6c7
Daaaaahling <una_cruz@yahoo.com>
2024-02-10 10:57:52 -0800

    added curly brackets

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -11,7 +11,7 @@ int _sqrt_recursion(int n)
 		return (-1);
 	}
 
-	return (find_sqrt(n, 1);
+	return (find_sqrt(n, 1));
 }
 
 /**
@@ -32,5 +32,5 @@ int find_sqrt(int n, int guess)
 	{
 		return (-1);
 	}
-	return (find_sqrt(n, guess + 1);
+	return (find_sqrt(n, guess + 1));
 }
