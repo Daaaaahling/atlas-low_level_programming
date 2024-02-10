743b8934755d7234b60ba0e8ccde20e91e441dc9
Daaaaahling <una_cruz@yahoo.com>
2024-02-09 17:43:59 -0800

    Write a function that returns the natural square root of a number.

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns natural square root
+ * @n: number for which to find the square root
+ * Return: square root if exists, if not -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return -1;
+	}
+
+	return find_sqrt(n, 1);
+}
+
+/**
+ * find_sqrt - helper function to recursively find square root
+ * @n: number for which to find the square root
+ * @guess: current guess for square root
+ * Return: square root if exists, if not -1
+ */
+int find_sqrt(int n, int guess)
+{
+	int square = guess * guess;
+
+	if (square == n)
+	{
+		return guess;
+	}
+	if (square > n || guess > n / guess)
+	{
+		return -1;
+	}
+	return find_sqrt(n, guess + 1);
+}
