8132ae39c04ef2eaf0316ea91ab3004793bef097
Daaaaahling <una_cruz@yahoo.com>
2024-02-12 11:14:43 -0800

    added -1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -5,10 +5,10 @@
  * @argv: array of arguments
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[])__attribute__((unused)))
 {
 	(void) argv;
 
-	printf("%d\n", argc);
+	printf("%d\n", argc -1);
 	return (0);
 }
