b118adb3777ce3be73868fe7ba9f920a8f2ab08b
Daaaaahling <una_cruz@yahoo.com>
2024-02-12 14:42:19 -0800

    betty spacing

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,14 +12,14 @@ int main(int argc, char *argv[])
 
     if (argc == 3)
     {
-        num1 = atoi(argv[1]);
-        num2 = atoi(argv[2]);
-        printf("%d\n", num1 * num2);
-        return (0);
+	    num1 = atoi(argv[1]);
+	    num2 = atoi(argv[2]);
+	    printf("%d\n", num1 * num2);
+	    return (0);
     }
     else
     {
-        printf("Error\n");
-        return (1);
+	    printf("Error\n");
+	    return (1);
     }
 }
