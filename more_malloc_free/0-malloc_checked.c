0fa28a1570f33aa9c7094927cd2621811e89bcb0
Daaaaahling <una_cruz@yahoo.com>
2024-02-18 13:55:39 -0800

    edit

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -5,17 +5,15 @@
  * malloc_checked - uses malloc to allocate memory
  * @b: number of bytes to allocate memory
  * Return: pointer to allocated memory
- * The malloc_checked function should cause normal process
- * termination with a status value of 98, if malloc fails
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
-	
-	ptr = malloc(b);
+	void *pointer;
 
-	if (ptr == NULL)
+	pointer = malloc(b);
+
+	if (pointer == NULL)
 		exit(98);
 
-	return (ptr);
+	return (pointer);
 }
