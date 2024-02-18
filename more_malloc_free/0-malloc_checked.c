e43bd295216070ab89aebedd4e29a28cd19b036e
Daaaaahling <una_cruz@yahoo.com>
2024-02-18 13:49:20 -0800

    spacing

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -5,12 +5,12 @@
  * malloc_checked - uses malloc to allocate memory
  * @b: number of bytes to allocate memory
  * Return: pointer to allocated memory
- * The malloc_checked function should cause normal process 
+ * The malloc_checked function should cause normal process
  * termination with a status value of 98, if malloc fails
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr; 
+	void *ptr;
 	
 	ptr = malloc(b);
 
