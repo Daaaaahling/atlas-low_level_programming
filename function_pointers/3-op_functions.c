4e0774cbba667e8c009be55904cf6234f98e0995
Daaaaahling <una_cruz@yahoo.com>
2024-02-24 16:20:12 -0800

    This file should contain the 5 following functions (not more)

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * op_add - adds two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - computes remainder of two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
