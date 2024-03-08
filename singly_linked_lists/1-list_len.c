80fa6089967ee1558192df7bd62babae50f79cc5
Daaaaahling <una_cruz@yahoo.com>
2024-03-08 23:00:50 +0000

    spacing

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -4,15 +4,14 @@
  * @h: list head
  * Return: number of elements in the linked list
  */
-unsigned int list_len(h)
-const list_t *h;
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	
+	while (h != NULL)
 	{
-		unsigned int count = 0;
-
-		while (h != NULL)
-		{
-			count++;
-			h = h->next;
+		count++;
+		h = h->next;
 	}
 	
 	return count;
