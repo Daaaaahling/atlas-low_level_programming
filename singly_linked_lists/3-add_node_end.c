2768a5320740d15a37937864a3e9dadbe38b7758
daaaaahling <una_cruz@yahoo.com>
2024-03-09 22:30:17 +0000

    spacing

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,53 +11,50 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new = NULL;
-    list_t *current = NULL;
-    unsigned int len = 0;
-
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-        return (NULL);
-
-    if (str != NULL)
-    {
-        while (str[len] != '\0')
-            len++;
-
-        new->str = malloc((len + 1) * sizeof(char));
-        if (new->str == NULL)
-        {
-            free(new);
-            return (NULL);
-        }
-
-        for (len = 0; str[len] != '\0'; len++)
-            new->str[len] = str[len];
-
-        new->str[len] = '\0';
-        new->len = len;
-    }
-    else
-    {
-        new->str = NULL;
-        new->len = 0;
-    }
-
-    new->next = NULL;
-
-    if (*head == NULL)
-    {
-        *head = new;
-        return (new);
-    }
-
-    current = *head;
-    while (current->next != NULL)
-    {
-        current = current->next;
-    }
-
-    current->next = new;
-
-    return (new);
+	list_t *new = NULL;
+	list_t *current = NULL;
+	unsigned int len = 0;
+	
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	
+	if (str != NULL)
+	{
+		while (str[len] != '\0')
+			len++;
+		
+		new->str = malloc((len + 1) * sizeof(char));
+		if (new->str == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+		for (len = 0; str[len] != '\0'; len++)
+			new->str[len] = str[len];
+		
+		new->str[len] = '\0';
+		new->len = len;
+	}
+	else
+	{
+		new->str = NULL;
+		new->len = 0;
+	}
+	
+	new->next = NULL;
+	
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	current = *head;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	
+	current->next = new;
+	return (new);
 }
