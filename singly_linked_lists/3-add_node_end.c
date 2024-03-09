e0cc330cc8a7918e6de945e9bcc577696dfd2270
daaaaahling <una_cruz@yahoo.com>
2024-03-09 22:21:25 +0000

    typo

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -24,7 +24,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		new->str = strdup(str);
 		if (new->str == NULL)
 		{
-			fre(new);
+			free(new);
 			return (NULL);
 		}
 		new->len = strlen(str);
