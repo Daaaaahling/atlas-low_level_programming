ace9323dac5c379ec8bc2a7804969e37687d31de
Daaaaahling <155494852+Daaaaahling@users.noreply.github.com>
2024-03-09 16:23:50 -0600

    without strlen

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -5,51 +5,59 @@
 /**
  * add_node_end - function that adds a new node at the end of a list_t list.
  * @head: head of list
- * @str: to be duplicated
+ * @str: string to be duplicated
  *
  * Return: the address of the new element, or NULL if it failed
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new = NULL;
-	list_t *current = NULL;
+    list_t *new = NULL;
+    list_t *current = NULL;
+    unsigned int len = 0;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+        return (NULL);
 
-	if (str != NULL)
-	{
-		new->str = strdup(str);
-		if (new->str == NULL)
-		{
-			free(new);
-			return (NULL);
-		}
-		new->len = strlen(str);
-	}
-	else
-	{
-		new->str = NULL;
-		new->len = 0;
-	}
+    if (str != NULL)
+    {
+        while (str[len] != '\0')
+            len++;
 
-	new->next = NULL;
+        new->str = malloc((len + 1) * sizeof(char));
+        if (new->str == NULL)
+        {
+            free(new);
+            return (NULL);
+        }
 
-	if (*head == NULL)
-	{
-		*head = new;
-		return (new);
-	}
+        for (len = 0; str[len] != '\0'; len++)
+            new->str[len] = str[len];
 
-	current = *head;
-	while (current->next != NULL)
-	{
-		current = current->next;
-	}
+        new->str[len] = '\0';
+        new->len = len;
+    }
+    else
+    {
+        new->str = NULL;
+        new->len = 0;
+    }
 
-	current->next = new;
+    new->next = NULL;
 
-	return (new);
+    if (*head == NULL)
+    {
+        *head = new;
+        return (new);
+    }
+
+    current = *head;
+    while (current->next != NULL)
+    {
+        current = current->next;
+    }
+
+    current->next = new;
+
+    return (new);
 }
