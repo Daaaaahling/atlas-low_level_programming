2f8ba7dcb42933474241fc9b37121538bc7e8e7d
daaaaahling <una_cruz@yahoo.com>
2024-03-11 23:44:38 +0000

    puncruation

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,9 +13,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	(
-		 while (text_content[length])
-		 length++;
-	}
+	 
+	 int length = 0;	
+	 while (text_content[length])
+	 {
+	 
+	 length++;
+	 }
 
 	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
@@ -23,10 +27,17 @@ int create_file(const char *filename, char *text_content)
 	return -1;
 
 	write_result = write(file_descriptor, text_content, length);
+	close(file_descriptor);
 
 	if (write_result == -1)
-	{
-	
+	return -1;
+
+	return 1;
+}
+file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+if (file_descriptor == -1)
+	return -1;
+
 	close(file_descriptor);
 
 	return 1;
