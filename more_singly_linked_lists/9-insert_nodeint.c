b2b6aabc1352afb3ff49277f1cd3b60396018fb4
daaaaahling <una_cruz@yahoo.com>
2024-03-11 12:08:52 +0000

    typo

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	current_node = *head;
-	for (u = 0; u < idx - 1 && current; u++)
+	for (u = 0; u < idx - 1 && current_node; u++)
 		current_node = current_node->next;
 
 	if (!current_node)
