229e3caefe4eb78624a942b257bca5383f3fab92
daaaaahling <una_cruz@yahoo.com>
2024-03-11 18:43:59 +0000

    typo

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -39,7 +39,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 
 	current_node->next = deleting_node->next;
-	free)deleting_node);
+	free(deleting_node);
 
 	return (1);
 }
