87aa231099621e6e5082217d5a784ba12dc0c261
daaaaahling <una_cruz@yahoo.com>
2024-03-12 00:03:08 +0000

    spacing

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,6 +2,7 @@
 /**
  * create_file - Creates a file and writes text content
  * @filename: pointer to the name of file to create
+ * @text_content: pointer to the text content to write to the file
  * Return: 1 on success, -1 if function fails
  */
 int create_file(const char *filename, char *text_content)
@@ -13,14 +14,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	(
-	 
-	 int length = 0;	
 	 while (text_content[length])
-	 }
-	
-	 {
 	 length++;
-	 }
+	 
 
 	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file_descriptor == -1)
