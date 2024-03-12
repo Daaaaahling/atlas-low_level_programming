b528ccb7f97c122e24ccfcfbb6f900688c179d69
daaaaahling <una_cruz@yahoo.com>
2024-03-12 03:05:31 +0000

    betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,38 +10,34 @@ int create_file(const char *filename, char *text_content)
 	int file_descriptor, write_result, length = 0;
 
 	if (filename == NULL)
-		return -1;
+		return (-1);
 
 	if (text_content != NULL)
 	{
 		while (text_content[length])
 			length++;
-	 
 
 	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file_descriptor == -1)
-		return -1;
+		return (-1);
 
 	write_result = write(file_descriptor, text_content, length);
 	close(file_descriptor);
 
 	if (write_result == -1)
-		return -1;
+		return (-1);
 	
-	return 1;
+	return (1);
 	
 	}
 	else
 	{
 		file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 		if (file_descriptor == -1)
-			return -1;
+			return (-1);
 
 	close(file_descriptor);
-	return 1;
+	return (1);
 	}
 }
-
-
-		
 
