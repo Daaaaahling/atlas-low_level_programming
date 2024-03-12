5217f6e5b16e466627251e9d1657e527ce3bdcc4
daaaaahling <una_cruz@yahoo.com>
2024-03-12 02:57:39 +0000

    spacing

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -58,35 +58,32 @@ int main(int argc, char *argv[])
 	
 	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
-	
-	if (from == -1)
-	{
-		dprintf(STDERR_FILENO,dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,dprintf(STDERR_FILENO,
+						"Error: Can't read from file %s\n", argv[1]);
 		free(buffer);
-		exit(99);
+		exit(98);
 		}
 		
-		to = open(argv[2], O_WRONLY | O_APPEND);
-		if (to == -1)
-		{
-	       
-		close_file(from);
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-		free(buffer);
-		exit(99);
-		}
-
 		w = write(to, buffer, r);
-
-		if (w == -1)
+		if (to == -1 || w == -1)
 		{
-
-		close_file(from);
-		close_file(to);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		
+		dprintf(STDERR_FILENO,
+				"Error: Can't write to file %s\n", argv[2]);
 		free(buffer);
 		exit(99);
-		}
+	}
+
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_WRONLY | O_APPEND);
+
+	
 } while (r > 0);
 
 free(buffer);
