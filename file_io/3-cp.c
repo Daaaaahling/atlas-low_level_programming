8a0faa67bbed63b7abe1b39f259392023549a444
daaaaahling <una_cruz@yahoo.com>
2024-03-12 01:11:38 +0000

    Write a program that copies the content of a file to another file.

---
+++ file_io/3-cp.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+#include <fcntl.h>
+
+char *allocate_buffer(char *filename);
+void close_file(int file_descriptor);
+/**
+ * create_buffer - allocates 1024 bytes
+ * @file: name of file buffer is storing chars for
+ * Return: pointer to the newly-allocated buffer
+ */
+char *allocate_buffer(char *filename)
+{
+	char *buffer = malloc(1024 * sizeof(char));
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
