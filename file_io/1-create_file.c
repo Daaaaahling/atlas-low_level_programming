d4892baae087a8bfd5a36ce99dd6478214280941
daaaaahling <una_cruz@yahoo.com>
2024-03-12 00:14:59 +0000

    punctuation

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,31 +13,35 @@ int create_file(const char *filename, char *text_content)
 		return -1;
 
 	if (text_content != NULL)
-	(
-	 while (text_content[length])
-	 length++;
+	{
+		while (text_content[length])
+			length++;
 	 
 
 	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file_descriptor == -1)
-	return -1;
+		return -1;
 
 	write_result = write(file_descriptor, text_content, length);
 	close(file_descriptor);
 
 	if (write_result == -1)
-	return -1;
-
-	return 1;
-}
-else
-{
-	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (file_descriptor == -1)
 		return -1;
+	
+	return 1;
+	
+	}
+	else
+	{
+		file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		if (file_descriptor == -1)
+			return -1;
 
 	close(file_descriptor);
 	return 1;
+	}
 }
+
+
 		
 
